b1da45c4c2482add6cc5b06afc96366c9a25cf6b
Antoni Sureda Riera <tonisr66@gmail.com>
2026-04-28 21:19:30 +0200

    guardar noms(temporal) i preparar extraccio regles

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner teclat = new Scanner(System.in);
 
 		Joc laMevaPartida = new Joc();
 
-		laMevaPartida.afegirJugadors("Nom1");
-		laMevaPartida.afegirJugadors("Nom2");
+		System.out.println("JUG 1");
+		laMevaPartida.afegirJugadors(teclat.nextLine());
+		System.out.println("JUG 2");
+		laMevaPartida.afegirJugadors(teclat.nextLine());
 
 		System.out.println("Preparant la baralla...");
 		laMevaPartida.crearBarallaEstandard();
--- src/ReglesJoc.java
+++ src/ReglesJoc.java
@@ -1,2 +1,9 @@
+import java.util.List;
+
 public interface ReglesJoc {
+	int pecesAlRepartir();
+
+	void inicialitzarPila(List<Peca> pila);
+
+	boolean haGuanyat(Jugador j);
 }
